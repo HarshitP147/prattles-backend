603fcbe5e01926cba9b7e31bcbffa42c9f3d0310
Harshit Pandit <harshit7757@gmail.com>
2024-06-27 00:59:20 +0530

    Minor chat file fix

--- routes/chat.js
+++ routes/chat.js
@@ -4,13 +4,13 @@ import Chat from '../models/Chat.js';
 
 const chatRoute = Router();
 
-
+// route to return all the messages
 chatRoute.get("/:chatId", async (req, res) => {
     const chatInfo = await Chat.findOne({
         chatId: req.params.chatId
     })
-
-    console.log(chatInfo);
+        .select('messages')
+        .populate('messages', 'sender content createdAt')
 
     res.status(200).json(chatInfo);
 })
